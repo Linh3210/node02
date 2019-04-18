08300f8f50b2c79d8ad60518f53aee085c4ab8db
Linh <phuonglinh02019979@gmail.com>
2019-04-18 19:19:49 +0700

    test

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const url = require('url');
+
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+var server = http.createServer(function(req,res){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if (req.url == "/hello")
+    {
+        if (req.method == "GET")
+        {
+            res.write("world");
+            res.end();
+        }
+        else if (req.method == "POST")
+        {
+            res.write("world created");
+            res.end();
+        }
+        else if (req.method == "PUT")
+        {
+            res.write("world updated");
+            res.end();
+        }
+        else if (req.method == "DELETE")
+        {
+            res.write("world deleted");
+            res.end();
+        }
+    }
+    else
+    {
+        res.write("URL not found!");
+    }
+});
+server.listen(port, host, function (err) {
+    if (!err) console.log(`Server started ${host} on ${port}`);
+    else console.log(err);
+});
